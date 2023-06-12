4da25ec967a1fd0d2c0e7386f5a02146f36ce76f
Mhaske Shubham <121010244+Mhaske1200@users.noreply.github.com>
2023-06-12 12:11:13 +0530

    Add files via upload

---
+++ Project_Office_Happiness_Report/User_Interface.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+print("-------------------WELCOME TO THE OFFICE HAPPINESS SOFTWARE--------------------")
+while True:
+    print("""
+    ------- MENU --------
+      SELECT ONE CHOICE
+      
+    1.Enter User Details
+    2.Enter Happiness Details
+    3.Get User Details
+    4.Get Happiness Details
+    5.Get User & Happiness Details on Gender
+    6.Fetch Users who are 😔
+    7.Fetch Users who are 😊
+    8.Exit""")
+    print("Choose any option from above menu:")
+    ch = int(input())
+    if ch == 1:
+        pass
+    elif ch==2:
+        pass
+    elif ch==3:
+        pass
+    elif ch==4:
+        pass
+    elif ch==5:
+        pass
+    elif ch==6:
+        pass
+    elif ch==7:
+        pass
+    elif ch==8:
+        break
+    else:
+        pass
